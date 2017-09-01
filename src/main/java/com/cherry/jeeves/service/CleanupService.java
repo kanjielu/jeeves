d83046eadba30995cfbff542e9d2f8787a6ac989
Kanjie Lu <lukanjie@gmail.com>
2017-09-01 14:46:44 +0800

    add cleanup code

---
+++ src/main/java/com/cherry/jeeves/service/CleanupService.java
@@ -0,0 +1,31 @@
+package com.cherry.jeeves.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CleanupService implements DisposableBean {
+
+    @Autowired
+    private WechatHttpService wechatHttpService;
+    @Autowired
+    private CacheService cacheService;
+
+    private static final Logger logger = LoggerFactory.getLogger(CleanupService.class);
+
+    @Override
+    public void destroy() throws Exception {
+        logger.warn("[*] system is being destroyed");
+        if (cacheService.isAlive()) {
+            try {
+                logger.warn("[*] logging out");
+                wechatHttpService.logout(cacheService.getHostUrl(), cacheService.getsKey());
+            } catch (Exception ex) {
+                logger.error(ex.getMessage(), ex);
+            }
+        }
+    }
+}
