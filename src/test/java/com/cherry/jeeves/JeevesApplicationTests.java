5291d81a5323404cb76edfed3c982782f5657dc4
Kanjie Lu <lukanjie@gmail.com>
2017-09-06 10:40:08 +0800

    fix tests2

--- src/test/java/com/cherry/jeeves/JeevesApplicationTests.java
+++ src/test/java/com/cherry/jeeves/JeevesApplicationTests.java
@@ -2,7 +2,6 @@ package com.cherry.jeeves;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -11,4 +10,8 @@ import java.io.IOException;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class JeevesApplicationTests {
+
+    @Test
+    public void contextLoads() throws IOException {
+    }
 }
--- src/test/java/com/cherry/jeeves/domain/response/TokenTest.java
+++ src/test/java/com/cherry/jeeves/domain/response/TokenTest.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class TokenTest {
 
     @Test
-    public void TestDeserializing() throws IOException {
+    public void TestDemoralizing() throws IOException {
         String xmlString = "<error><ret>0</ret><message></message><skey>@crypt_c51c94ad_06b82d2842bdefdae788fbb762d9b631</skey><wxsid>QT6wafhptuVx0gKa</wxsid><wxuin>1983611971</wxuin><pass_ticket>i8PQXWfnMmVX%2Frx0ThkPwapCC0jBOMxV7cjYdgFrW1leTJ28vqDXBzyqkt3RLjbM</pass_ticket><isgrayscale>1</isgrayscale></error>";
         ObjectMapper xmlMapper = new XmlMapper();
         Token token = xmlMapper.readValue(xmlString, Token.class);
